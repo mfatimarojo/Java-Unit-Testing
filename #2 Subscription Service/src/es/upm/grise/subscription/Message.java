352b84abb6eab3dae11d375443eb5c444c6e1a82
Fátima Rojo <11411647+serfati5@users.noreply.github.com>
2019-10-04 10:50:56 -0500

    Add files via upload

---
+++ #2 Subscription Service/src/es/upm/grise/subscription/Message.java
@@ -0,0 +1,5 @@
+package es.upm.grise.subscription;
+
+public interface Message {
+
+}
---
+++ #2 Subscription Service/src/es/upm/grise/subscription/NonExistingClientException.java
@@ -0,0 +1,5 @@
+package es.upm.grise.subscription;
+
+public class NonExistingClientException extends Exception {
+
+}
